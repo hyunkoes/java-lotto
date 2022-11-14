55f13031b0ca60cff29227d89c48df7698ae65c6
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-14 21:18:51 +0900

    chore: BONNUS -> BONUS

--- src/main/java/lotto/contant/LottoConst.java
+++ src/main/java/lotto/contant/LottoConst.java
@@ -4,7 +4,7 @@ public class LottoConst {
     public static final int LOTTO_NUM_START = 1;
     public static final int LOTTO_NUM_END = 45;
     public static final int WINNING_NUM_SIZE = 6;
-    public static final int BONNUS_NUM_SIZE = 1;
+    public static final int BONUS_NUM_SIZE = 1;
     public static final int LOTTO_FEE = 1000;
     public static final int FIRST_PRIZE = 2000000000;
     public static final int SECOND_PRIZE = 30000000;
@@ -15,5 +15,5 @@ public class LottoConst {
     public static final String BUYING_MESSAGE = "개를 구매했습니다.\n";
     public static final String WINNIG_NUM_SET_MESSAGE = "당첨 번호를 입력해주세요.\n";
     public static final String BONNUS_NUM_SET_MESSAGE = "보너스 번호를 입력해주세요.\n";
-    public static final String RESULT_MESSAGE = "당첨 통계\n";
+    public static final String RESULT_HEADER_MESSAGE = "당첨 통계\n---\n";
 }
