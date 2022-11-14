d3d6394dc77b90bffa0d52a23b25bb133562e738
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-14 20:43:00 +0900

    fix: PriorityQueue -> Collections.sort(ArrayList)

--- src/main/java/lotto/util/RandomGenerator.java
+++ src/main/java/lotto/util/RandomGenerator.java
@@ -1,20 +1,22 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.contant.LottoConst;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.ArrayList;
 
 public class RandomGenerator {
-    public static List<Integer> generateRandomNums(int size){
-        Queue<Integer> randoms = new PriorityQueue<>();
-        while (randoms.size() < size) {
+    public static List<Integer> generateRandomNums(){
+        List<Integer> randoms = new ArrayList<>();
+        while (randoms.size() < LottoConst.WINNING_NUM_SIZE) {
             int randomNumber = Randoms.pickNumberInRange(1, 45);
             if (!randoms.contains(randomNumber)) {
                 randoms.add(randomNumber);
             }
         }
-        return (List) randoms;
+        Collections.sort(randoms);
+        return randoms;
     }
 }
