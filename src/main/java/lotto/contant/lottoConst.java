2ea6e62b2015e0af2667d68c1aad6e41cbbce4a2
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-14 19:35:34 +0900

    feat: Initialize lotto contants

---
+++ src/main/java/lotto/contant/lottoConst.java
@@ -0,0 +1,16 @@
+package lotto.contant;
+
+public class lottoConst {
+    public static final int WINNING_NUM_SIZE = 6;
+    public static final int BONNUS_NUM_SIZE = 1;
+    public static final int FIRST_PRIZE = 2000000000;
+    public static final int SECOND_PRIZE = 30000000;
+    public static final int THIRD_PRIZE = 1500000;
+    public static final int FOURTH_PRIZE = 50000;
+    public static final int FIFTH_PRIZE = 5000;
+    public static final String START_MESSAGE = "구입금액을 입력해 주세요.\n";
+    public static final String BUYING_MESSAGE = "개를 구매했습니다.\n";
+    public static final String WINNIG_NUM_SET_MESSAGE = "당첨 번호를 입력해주세요.\n";
+    public static final String BONNUS_NUM_SET_MESSAGE = "보너스 번호를 입력해주세요.\n";
+    public static final String RESULT_MESSAGE = "당첨 통계\n";
+}
