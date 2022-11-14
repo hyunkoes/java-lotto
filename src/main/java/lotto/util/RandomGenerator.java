5a02f922160164f8aef792e7ac0c6b1cf47f90fc
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-14 22:19:57 +0900

    chore: (1, 45) -> use constants

--- src/main/java/lotto/util/RandomGenerator.java
+++ src/main/java/lotto/util/RandomGenerator.java
@@ -11,7 +11,7 @@ public class RandomGenerator {
     public static List<Integer> generateRandomNums(){
         List<Integer> randoms = new ArrayList<>();
         while (randoms.size() < LottoConst.WINNING_NUM_SIZE) {
-            int randomNumber = Randoms.pickNumberInRange(1, 45);
+            int randomNumber = Randoms.pickNumberInRange(LottoConst.LOTTO_NUM_START, LottoConst.LOTTO_NUM_END);
             if (!randoms.contains(randomNumber)) {
                 randoms.add(randomNumber);
             }
