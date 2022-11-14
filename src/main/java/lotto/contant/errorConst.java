852a4504c429ede8fe658a7fb984dd71c72d1b78
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-14 19:44:33 +0900

    feat: Initialize error message contants

---
+++ src/main/java/lotto/contant/errorConst.java
@@ -0,0 +1,9 @@
+package lotto.contant;
+
+public class errorConst {
+    public static final String NOT_A_NUM = "숫자를 입력해야 합니다.";
+    public static final String INVALID_FEE = lottoConst.LOTTO_FEE+"단위로 구입해야 합니다.";
+    public static final String NOT_IN_NUM_RANGE = "1 ~ 45 사이의 로또 번호를 입력해야 합니다.";
+    public static final String NOT_UNIQUE_NUMS = "중복이 없는 번호를 입력해주세요.";
+    public static final String NOT_ENOUGH_NUMS = lottoConst.WINNIG_NUM_SET_MESSAGE+lottoConst.BONNUS_NUM_SIZE+"개의 번호를 입력해야 합니다.";
+}
