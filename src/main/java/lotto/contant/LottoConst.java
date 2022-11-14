74116034af0ac6c3206f933eaec76a3a8eeb882d
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-14 22:57:10 +0900

    feat: print lotto result(statistic)

--- src/main/java/lotto/contant/LottoConst.java
+++ src/main/java/lotto/contant/LottoConst.java
@@ -1,5 +1,7 @@
 package lotto.contant;
 
+import lotto.util.UnitConverter;
+
 public class LottoConst {
     public static final int LOTTO_NUM_START = 1;
     public static final int LOTTO_NUM_END = 45;
@@ -10,14 +12,20 @@ public class LottoConst {
     public static final int SECOND_THIRD_CONDITION = 5;
     public static final int FOURTH_CONDITION = 4;
     public static final int FIFTH_CONDITION = 3;
-    public static final int FIRST_PRIZE = 2000000000;
-    public static final int SECOND_PRIZE = 30000000;
-    public static final int THIRD_PRIZE = 1500000;
-    public static final int FOURTH_PRIZE = 50000;
-    public static final int FIFTH_PRIZE = 5000;
+    public static final String FIRST_PRIZE = UnitConverter.convertWonNotation("2000000000");
+    public static final String SECOND_PRIZE = UnitConverter.convertWonNotation("30000000");
+    public static final String THIRD_PRIZE = UnitConverter.convertWonNotation("1500000");
+    public static final String FOURTH_PRIZE = UnitConverter.convertWonNotation("50000");
+    public static final String FIFTH_PRIZE = UnitConverter.convertWonNotation("5000");
     public static final String START_MESSAGE = "구입금액을 입력해 주세요.\n";
     public static final String BUYING_MESSAGE = "개를 구매했습니다.\n";
     public static final String WINNING_NUM_SET_MESSAGE = "당첨 번호를 입력해주세요.\n";
     public static final String BONUS_NUM_SET_MESSAGE = "보너스 번호를 입력해주세요.\n";
     public static final String RESULT_HEADER_MESSAGE = "당첨 통계\n---\n";
+    public static final String[] WINNING_MESSAGE = {
+            FIFTH_CONDITION + "개 일치 (" + FIFTH_PRIZE + ")",
+            FOURTH_CONDITION + "개 일치 (" + FOURTH_PRIZE + ")",
+            SECOND_THIRD_CONDITION + "개 일치 (" + THIRD_PRIZE + ")",
+            SECOND_THIRD_CONDITION + "개 일치, 보너스 볼 일치 (" + SECOND_PRIZE + ")",
+            FIRST_CONDITION + "개 일치 (" + FIRST_PRIZE + ")"};
 }
--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -3,15 +3,22 @@ package lotto.view;
 import lotto.contant.LottoConst;
 import lotto.domain.Lotto;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class View {
     public static void printMessage(String message){
-        System.out.print(message);
+        System.out.print("\n"+message);
     }
     public static void printLottoInfo(List<Lotto> lottos) {
         for (int i = 0; i < lottos.size(); i++) {
             System.out.println(lottos.get(i).toString());
+        }
+    }
+    public static void printLottoResult(List<Integer> winningResults){
+        System.out.print("\n"+LottoConst.RESULT_HEADER_MESSAGE);
+        for ( int i = 0 ; i < winningResults.size() ; i ++ ){
+            System.out.println(LottoConst.WINNING_MESSAGE[i] + " - " + winningResults.get(i)+ "개");
         }
     }
 }
